cb600f381e45c5532227cc395e35a3750811fee9
Miroslaw Ratman <miroslaw.ratman@avast.com>
2021-09-21 18:00:06 +0200

    init commit

---
+++ src/Modules/Api.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import express from 'express';
+var app = express();
+
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+})
+
+
+
+
+
+module.export app;
